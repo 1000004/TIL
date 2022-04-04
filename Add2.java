1d474af64afa1841f89444d821dc19faf66c513c
git <forger980@gmail.com>
2022-04-04 21:15:12 +0900

    Second commit

--- Add2.java
+++ Add2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 //동일한 클래스 이름 충돌 방지
 //유일한 문자열
 //Java에서 만든 API를 제외하고 도메인 이름을 뒤집어서 쓴다.
-public class add2 {
+public class Add2 {
     public static void main(String args[]){
 
         Scanner input = new Scanner(System.in);
@@ -18,7 +18,7 @@ public class add2 {
         int y;
         int sum;
 
-        System.out.ptint("첫번째 숫자를 입력하시오: ");
+        System.out.print("첫번째 숫자를 입력하시오: ");
         //System.out 표준입력장치(콘솔:입출력장치)
         x = input.nextInt();
 
@@ -27,6 +27,7 @@ public class add2 {
 
         sum = x + y;
         
+        System.out.println(sum);
 
     }
 }
