e2e2df6e66187d98c1d781ce4cf24f1c6fcb7ace
git <forger980@gmail.com>
2022-04-10 17:11:51 +0900

    4월10일commit

---
+++ programBeginner/Weight.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Weight  {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("키를 입력하오(cm): ");
+		int h = scan.nextInt();
+		System.out.print("체중을 입력하시오(kg): ");
+		int w = scan.nextInt();
+		double weight = (h -100)*0.9;
+		String stWeight = "표준";
+		int gap = 5; //표준 범위 설정
+		if (w>(weight + gap)){
+			stWeight = "과체중";
+		}
+		else if (w<(weight - gap)){
+			stWeight = "저체중";
+		}
+		System.out.println("표준체중 : " + weight);
+		System.out.println("당신은 " + stWeight + " 입니다.");
+			
+	}
+}
+
