fde1d7b7be406383ddf3fb653950abc5f8ea22eb
git <forger980@gmail.com>
2022-04-14 11:54:49 +0900

    4월14일commit

--- JujutsukaisenMovie.java
+++ JujutsukaisenMovie.java
@@ -1,14 +1,44 @@
 class Movie{
-	String title;
-	double rating;
-	String director;
-	int koreaReleaseYear;
+	private String title;
+	private double rating;
+	private String director;
+	private int koreaReleaseYear;
 
-	void print(){
-		String info = "Movie title : \t" + title + "\n";
-		info +="rating : \t"+rating+"\n";
-		info +="director : \t"+director+"\n";
-		info +="year :    \t"+koreaReleaseYear+"\n";
+	//getter
+	public String getTitle(){
+		return title;
+	}
+	//setter
+	public void setTitle(String title){
+		this.title =title;
+	}
+
+	public double getRating(){
+		return rating;
+	}
+	public void setRating(double rating){
+		this.rating = rating;
+	}
+
+	public String getDirector(){
+		return director;
+	}
+	public void setDirector(String director){
+		this.director = director;
+	}
+	
+	public int getKoreaReleaseYear(){
+		return koreaReleaseYear;
+	}
+	public void setKoreaReleaseYear(int koreaReleaseYear){
+		this.koreaReleaseYear = koreaReleaseYear;
+	}
+	
+	public void print(){
+		String info = "Movie title \t: "+ title + "\n";
+		info +="rating \t\t: "+rating+"\n";
+		info +="director \t: "+director+"\n";
+		info +="year \t\t: " + koreaReleaseYear;
 		System.out.println(info);
 	}
 }
@@ -17,10 +47,10 @@ public class  JujutsukaisenMovie {
 	public static void main(String[] args) {
 		
 		Movie m = new Movie();
-		m.title = "jujutsu kaisen 0";
-		m.rating = 9.02;
-		m.director = "Park Sunghoo";
-		m.koreaReleaseYear = 2022;
+		m.setTitle("jujutsu kaisen 0");
+		m.setRating(9.02);
+		m.setDirector("Park Sunghoo");
+		m.setKoreaReleaseYear(2022);
 		m.print();
 
 
---
+++ UseHuman.java
@@ -0,0 +1,64 @@
+class Human {
+	private String name;
+	private int age;
+	private String tel = "010-5409-8756";
+	private boolean female;	//남자 :false, 여자 : true
+	private boolean feelSoGood = true;
+	//getter/setter
+	//getter
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
+		//boolean is~
+	public boolean isFemale(){
+		return female;
+	}
+	public String getTel(){
+		if(feelSoGood){
+			return tel;
+		}else{
+			return "112";
+		}
+	}
+	//setter
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public void setFemale(boolean feamale){
+		this.female = female;
+	}
+
+	public void printInfo(){
+		String info = "name : " + name + "\n";
+		info += "age" + age + "\n";
+		info += "female : " + female;
+
+		System.out.println(info);
+	}
+}
+
+public class  UseHuman {
+	public static void main(String[] args) {
+		Human h = new Human();
+		h.setName("춘식");
+		h.setAge(3);
+		h.setFemale(true);
+
+		h.printInfo();
+
+		// h.age = 20; 외부에서 마음데로 바꿈
+		h.setAge(20); // 어디서 누가 바꾸는가 알수 있다 객체 내부에서 소유자가 결정권을 가짐
+
+		String humanName = h.getName();
+		System.out.println(humanName);
+		
+
+
+	}
+}
