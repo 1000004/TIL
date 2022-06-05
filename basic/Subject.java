0ba1974f7ed9843f06a5b2e0c3c269420229c39f
1000004 <102463200+1000004@users.noreply.github.com>
2022-06-05 22:47:52 +0900

    _

--- basic/Subject.java
+++ basic/Subject.java
@@ -38,7 +38,7 @@ class Subject{
 			return false;
 		}
 		Subject temp = (Subject)o;
-		return name.equals(temp.getName());
+		return name.equals(temp.getName());//비교대상의 String name 멤버변수의 내용이 같으면 같은 교과목
 	}
 }
 class Student{
@@ -66,7 +66,7 @@ class Student{
 	public int getTotal(){
 		int total =0;
 		for(Subject temp : scores){
-			total += temp.getScore();
+			total += temp.getScore();//Student 클래스에 포함된 Subject 배열 모든 항목의 점수를 가져와 합한다.
 		}
 		return total;
 	}
@@ -83,21 +83,21 @@ class Student{
 		Subject goal = new Subject(name);
 		int score = -1;
 		for(Subject temp : scores){
-			if(temp.equals(goal)){
-				score = temp.getScore();
+			if(temp.equals(goal)){//Student 클래스에 포함된 Subject 배열 항목을 대상으로 equals method를 통한 동일한 이름을 가진 과목인 경우 ture
+				score = temp.getScore();//참이면 해당 Subject 점수를 가져온다
 			}
 		}
-		return score;
+		return score;//결과적으로 파라미터 name와 동일한 Subject name을 가진 점수를 반환한다.
 	}
 
 	@Override
-	public string toString(){
+	public String toString(){
 		String info = "<<" + name + ">>";
 		for(Subject temp : scores){
-			info += temp.toSring() + "\n";
+			info += temp.toString() + "\n";
 		}
 		info += "총점 : " +getTotal() + "\n";
-		info += "평균 : " +getAng();
+		info += "평균 : " +getAvg();
 
 		return info;
 	}
@@ -109,7 +109,7 @@ class Klass{
 	public Klass(Student...list){
 		setList(list);
 	}
-	public student[] getList(){
+	public Student[] getList(){
 		return list;
 	}
 	public void setList(Student...list){
@@ -118,8 +118,8 @@ class Klass{
 	//교과목 총점
 	public int getTotal(String subjectName){
 		int total = 0;
-		for(Student temp : list){
-			int score = temp.getSubjectScore(subjectName);
+		for(Student temp : list){//모든 학생리스트를 대상으로 원하는 과목의 점수를 찾는다.
+			int score = temp.getSubjectScore(subjectName);//파라미터 subjectName와 동일한 과목이름인 경우 가진 점수를 가져온다.
 			if(score >=0){
 				total +=score;
 			}
@@ -133,9 +133,9 @@ class Klass{
 
 	@Override
 	public String toString(){
-		String info = "- 학급정보 -\n"
+		String info = "- 학급정보 -\n";
 		for(Student temp : list){
-			info += tem.toString() + "\n"
+			info += temp.toString() + "\n";
 		}
 		//같은 학급의 학생은 시험 교과가 같으므로
 		Subject[] scores = list[0].getScores();
@@ -155,7 +155,7 @@ class Answer{
 		Subject s3 = new Subject("수학", 100);
 
 		Student st1 = new Student("애복이",s1,s2,s3);
-		Student st2 = new Student{
+		Student st2 = new Student(
 			"김수현",
 			new Subject("국어", 90),
 			new Subject("영어", 100),
@@ -166,7 +166,7 @@ class Answer{
 			new Subject("국어", 100),
 			new Subject("영어", 100),
 			new Subject("수학", 100)
-		};
+		);
 
 		System.out.println(new Klass(st1, st2, st3));
 	}
