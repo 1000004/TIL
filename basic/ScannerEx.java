9b5a8a3ecaa7ef3307b78bd438f42277e29736b5
1000004 <102463200+1000004@users.noreply.github.com>
2022-06-05 23:04:24 +0900

    _

---
+++ basic/ScannerEx.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class ScannerEx {
+	final static int STUDENTS = 5;
+
+	public static void main(String[] args){
+		int[] scores = new int[STUDENTS];
+		getValues(scores);
+		getAverage(scores);
+	}
+	public static void getValues(int[] array) {
+		Scanner scan = new Scanner(System.in);
+		for(int i = 0; i < array.length; i++){
+			System.out.print("성적을 입력하시오: ");
+			array[i] = scan.nextInt();
+		}
+	}
+	private static void getAverage(int[] array){
+		int total = 0;
+		for(int i =0; i < array.length; i++){
+			total += array[i];
+		}
+		System.out.println("평균 성적은 " + total/array.length + "입니다.");
+	}
+}
