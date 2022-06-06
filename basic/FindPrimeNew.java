a572d4970179f2302fcc468e6881831d01cee46b
1000004 <102463200+1000004@users.noreply.github.com>
2022-06-06 22:00:31 +0900

    _

--- basic/FindPrimeNew.java
+++ basic/FindPrimeNew.java
@@ -2,9 +2,9 @@ import java.util.*;
 class FindPrimeNew {
 	static int count = 0;
 	public static Integer[] getPrimes(int end){
-		vector<Integer> primes = new Vector<Integer>();
+		Vector<Integer> primes = new Vector<Integer>();
 		for(int i=2; i<=end; i++){
-			booleam isPrime =true;
+			boolean isPrime =true;
 			for(int idx=0; isPrime && idx<primes.size() && primes.get(idx) <= i/2; idx++){
 				if(i%primes.get(idx)==0){
 					isPrime = false;
@@ -21,7 +21,7 @@ class FindPrimeNew {
 	public static void main(String[] args) {
 		//2~100까지 소수를 구한다.
 		Integer[] nums = getPrimes(100);
-		for(int n : nums{
+		for(int n : nums){
 			System.out.println(n);
 		}
 		System.out.println("count: " + count);
