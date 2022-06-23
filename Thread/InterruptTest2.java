c4749a1aea6f42c17a83bd2662593e4cc64c1b75
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-06-23 17:48:39 +0900

    _

---
+++ Thread/InterruptTest2.java
@@ -0,0 +1,33 @@
+public class InterruptTest2 {
+	public static void main(String[] args) {
+		Thread t = new Thread(){
+			public void run(){
+				try {
+					System.out.println(
+						"try - isInterrupted : "+isInterrupted()
+					);
+					//인터럽트 받은 기록이 초기화됨(flas)
+					System.out.println(
+						"try - isInterrupted : "+ Thread.interrupted()
+						//static 메서드 true/false interrupt() 받은 기록 false로 변경
+					);
+					System.out.println("ok");
+					Thread.sleep(4000);
+					//interrupt()했음에도 Thread.interrupted()로 인해 4초동안 잠든다
+				} catch (InterruptedException e) {
+					System.out.println(
+						"catch - isInterrupted : "+ isInterrupted()
+					);
+				}
+			}
+		};
+		t.start();
+		t.interrupt();
+		
+		System.out.println("isInterrupted : "+t.isInterrupted());
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {}
+		System.out.println("isInterrupted : "+ t.isInterrupted());
+	}
+}
