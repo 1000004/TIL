8fee6bfeca2cb30a66ba4b9b3f5f28e2c548a150
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-06-28 13:18:18 +0900

    _

--- Network/echo-thread_server/EchoThreadServer.java
+++ Network/echo-thread_server/EchoThreadServer.java
@@ -8,7 +8,7 @@ public class EchoThreadServer {
 			//10001 port 번호 (프로세스 식별자 port)
 			//10001 port로 받겠다
 			System.out.println("접속을 기달립니다.");
-			while (true) {//*접속 대기 하나여서 하나만 접속 가능 -> 반복문
+			while (true) {//*접속 대기 하나여서 하나만 접속 가능(bindException 주소를 이미 사용중 (port)) -> 반복문 여러 client 접속
 				//client 정보를 담긴 소켓
 				Socket sock = server.accept();//*통신하고 있는 대상의 정보는 소켓이가지고 있다
 				//1)접속을 대기 클라이언트에 접속을 받을 준비 쓰레드가 정지 (실행 불가상태) 접속 할때 까지 대기
