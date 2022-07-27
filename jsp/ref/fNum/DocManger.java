0c128eb11573cd41546954d2f4eec3aa30a2c8e4
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-07-27 14:42:00 +0900

    _

---
+++ jsp/ref/fNum/DocManger.java
@@ -0,0 +1,22 @@
+package kr.ac.green;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+
+public class DocManger {
+	//목록가져오기
+	public static Vector<Doc> getList(ServletContext application){
+		Vector<Doc> list = (Vector<Doc>)application.getAttribute("docList");
+		if(list == null){
+			list = new Vector<Doc>();
+			application.setAttribute("docList", list);
+		}
+		return list;
+	}
+	//글등록
+	public static void addDoc(Doc doc, ServletContext application){
+		Vector<Doc> docList = getList(application);
+		docList.add(doc);
+	}
+}
