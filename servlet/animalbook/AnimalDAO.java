b692e0073b34259c7f64bc130d1178207fb9840e
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-08-04 20:49:41 +0900

    _

---
+++ servlet/animalbook/AnimalDAO.java
@@ -0,0 +1,27 @@
+package kr.ac.green;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+public class AnimalDAO {
+	private static ServletContext application;
+	
+	public static void init(ServletContext app){
+		application = app;
+		application.setAttribute("list", new Vector<Animal>());
+	}
+	/*
+	 * length -> 0
+	 */
+	public static Animal[] getList() {
+		Vector<Animal> list = (Vector<Animal>)application.getAttribute("list");
+		return list.toArray(new Animal[0]);
+	}
+	public static void addAnimal(Animal animal){
+		Vector<Animal> list = (Vector<Animal>)application.getAttribute("list");
+		list.add(animal);
+	}
+}
