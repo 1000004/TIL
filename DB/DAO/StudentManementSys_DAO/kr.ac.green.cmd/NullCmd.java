641c5b1c434725af8646d4bf0b20b16c81802acf
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-08-10 20:57:41 +0900

    _

---
+++ DB/DAO/StudentManementSys_DAO/kr.ac.green.cmd/NullCmd.java
@@ -0,0 +1,17 @@
+package kr.ac.green.cmd;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class NullCmd implements ICmd {
+
+	private String nextPage;
+	public NullCmd(String nextPage){
+		this.nextPage = nextPage;
+	}
+	
+	@Override
+	public String action(HttpServletRequest requset) {
+		return nextPage;
+	}
+
+}
