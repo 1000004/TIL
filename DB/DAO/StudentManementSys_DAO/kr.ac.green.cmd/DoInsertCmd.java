8e52158663091334fc59397e844b40848457c4d3
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-08-10 20:54:26 +0900

    _

---
+++ DB/DAO/StudentManementSys_DAO/kr.ac.green.cmd/DoInsertCmd.java
@@ -0,0 +1,32 @@
+package kr.ac.green.cmd;
+
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import kr.ac.green.dao.DAOProvider;
+import kr.ac.green.dao.IDAO;
+import kr.ac.green.dao.StudentDAO;
+import kr.ac.green.dto.Student;
+
+public class DoInsertCmd implements ICmd {
+
+	@Override
+	public String action(HttpServletRequest requset) {
+		String s_id=requset.getParameter("s_id");
+		String s_name=requset.getParameter("s_name");
+		int s_age = Integer.parseInt(requset.getParameter("s_age"));
+		String s_class = requset.getParameter("s_class");
+		String s_gender = requset.getParameter("s_gender");
+		Student s = new Student(s_id, s_name, s_age, s_class, s_gender);
+		
+		IDAO dao = DAOProvider.getDao();
+		Connection con = dao.connect();
+		dao.insert(con, s);
+		dao.disconnect(con);
+		
+		requset.setAttribute("isRedirect", true);
+		return "getAll.do";
+	}
+
+}
