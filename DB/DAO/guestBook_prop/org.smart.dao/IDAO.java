b8edc5ce9d8caa07aa524239ea3e1f0896533126
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-08-12 21:07:38 +0900

    _

---
+++ DB/DAO/guestBook_prop/org.smart.dao/IDAO.java
@@ -0,0 +1,12 @@
+package org.smart.dao;
+
+import java.sql.Connection;
+
+import org.smart.bean.GuestBook;
+
+public interface IDAO {
+	Connection connect();
+	int write(Connection con, GuestBook book);
+	GuestBook[] readBook(Connection con);
+	void disconnect(Connection con);
+}
