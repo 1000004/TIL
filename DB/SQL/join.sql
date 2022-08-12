fe96cf4295828d65346e86568072cbc3a9af2437
yeRim <102463200+yeRim650@users.noreply.github.com>
2022-08-12 21:45:48 +0900

    _

--- DB/SQL/join.sql
+++ DB/SQL/join.sql
@@ -4,6 +4,42 @@ SELECT * FROM orders;
 SELECT * FROM products;
 SELECT * FROM vendors;
 
+-- sub-query 와 JOIN
+SELECT * FROM products WHERE prod_name = 'King doll';
+SELECT vend_id
+	FROM products
+	WHERE prod_name = "King doll";
+-- sub-query
+-- 유일한 값으로 보장하지 않는다
+-- (p.k(null 불가능), 유니크(중복이 아닌 열 null 가능)로 찾는 경우 유일 값)
+-- = 는 1대1인 경우  연산 가능(= 대신 IN을 사용 vend_id가 여러개 or)
+SELECT vend_name, vend_address
+	FROM vendors
+	WHERE vend_id IN (SELECT vend_id
+							FROM products
+							WHERE prod_name = 'King doll');
+SELECT vend_name, vend_address
+FROM
+	vendors, products
+WHERE
+ vendors.vend_id = products.vend_id
+ AND
+ prod_name = 'King doll';
+SELECT vend_name, vend_address
+FROM
+ 	vendors INNER JOIN products
+ON
+ 	vendors.vend_id = products.vend_id
+WHERE
+ 	prod_name='King doll';
+SELECT vend_name, vend_address
+FROM
+	vendors NATURAL JOIN products
+WHERE
+	prod_name='King doll'
+-- 결합 조건이 없다
+-- NATURAL JOIN 외래키, 주키의 열 이름이 동일하는 경우 결합 조건 생략 가능
+
 SELECT DISTINCT cust_name
 FROM
 	customers NATURAL JOIN orders NATURAL JOIN
